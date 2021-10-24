a5155178a434ba2aa764fe0c3900c8efb8c9a15d
Daniel Lowry <daniel@daniellowry.co.uk>
2021-10-24 20:58:16 +0100

    Added dependency checking in module install script

--- install_pam-qr.sh
+++ install_pam-qr.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 #This script installs pam-qr and restarts the ssh service
 
+if ! command -v gcc &> /dev/null
+then
+    echo "Installing GCC..."
+    apt install gcc
+fi
+
+if ! command -v ld &> /dev/null
+then
+    echo "Installing bintuils..."
+    apt install bintuils
+fi
+
+if ! command -v libpam0g-dev &> /dev/null
+then
+    echo "Installing libpam0g-dev..."
+    apt install libpam0g-dev
+fi
+
 gcc -fPIC -fno-stack-protector -c src/pam-qr.c
 
 sudo rm /lib/x86_64-linux-gnu/security/pam-qr.so
