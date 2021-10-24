f767e2739325d4b6b0557e138c027bbb3af5ffe6
Daniel Lowry <daniel@daniellowry.co.uk>
2021-10-24 21:08:44 +0100

    Bug fix for module and script
    - Corrected incorrect paths in install script
    - Corrected misnaming of authenticate function

--- install_pam-qr.sh
+++ install_pam-qr.sh
@@ -23,8 +23,8 @@ gcc -fPIC -fno-stack-protector -c src/pam-qr.c
 
 sudo rm /lib/x86_64-linux-gnu/security/pam-qr.so
 sudo ld -x --shared -o /lib/x86_64-linux-gnu/security/pam-qr.so pam-qr.o 
-sudo chmod 644 pam-qr.so
+sudo chmod 644 /lib/x86_64-linux-gnu/security/pam-qr.so
 
-rm pam-qr.so
+rm pam-qr.o
 
 sudo systemctl restart sshd
--- src/pam-qr.c
+++ src/pam-qr.c
@@ -24,7 +24,7 @@ int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
 }
 
 /* PAM entry point for authentication verification */
-int pam_sm_authentication(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     return(PAM_IGNORE);
 }
 
